df688044584db929cd48476238a1f1c333c82a5e
level-one-ai <dean@levelone.digital>
2026-02-11 13:39:31 +0000

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -283,9 +283,12 @@ hexBurger.addEventListener('click', () => {
     const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
     document.body.style.overflow = 'hidden';
     document.body.style.paddingRight = `${scrollbarWidth}px`;
+    // Also apply padding to fixed elements to prevent shift
+    document.querySelector('nav').style.paddingRight = `calc(2.5rem + ${scrollbarWidth}px)`;
   } else {
     document.body.style.overflow = 'auto';
     document.body.style.paddingRight = '0px';
+    document.querySelector('nav').style.paddingRight = '2.5rem';
   }
   
   if (!isOpen && currentMenuLevel === 'subsection') {
